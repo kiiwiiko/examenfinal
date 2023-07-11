7f5d63cf9110e4e978fe0cacba0786d1b180579d
Arie Quimi <trr.2002j@gmail.com>
2023-07-11 12:33:07 -0500

    funcion para corregir las faltas de ortografia

--- programa.c
+++ programa.c
@@ -1,17 +1,30 @@
 #include <stdio.h>
 #include <conio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 //funcion para corregir la falta
-void CorregirFalta()
-{
+int CorregirOrtografia(FILE *archivoEntrada, FILE *archivoSalida) {
+    char palabra[100];
+    int conteoCorrecciones = 0;
 
+    while (fscanf(archivoEntrada, "%s", palabra) != EOF) {
+        if (strcmp(palabra, "ecuador") == 0) {
+            palabra[0] = toupper(palabra[0]);
+            fprintf(archivoSalida, "%s ", palabra);
+            conteoCorrecciones++;
+        } else {
+            fprintf(archivoSalida, "%s ", palabra);
+        }
+    }
+
+    return conteoCorrecciones;
 }
 
 //funcion main del programa
-int main()
+int main() 
 {
-
     getch();
     return 0;
 }
