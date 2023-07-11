2d7d48afc01c3fb73c63f592ed4fb1c70e876cbf
Arie Quimi <trr.2002j@gmail.com>
2023-07-11 12:42:47 -0500

    Se ocupa la funcion en la funcion main y se agrga restricciones

--- programa.c
+++ programa.c
@@ -8,23 +8,46 @@
 int CorregirOrtografia(FILE *archivoEntrada, FILE *archivoSalida) {
     char palabra[100];
     int conteoCorrecciones = 0;
-
+//una secuencia para que el programa busque la palabra y la cambie
     while (fscanf(archivoEntrada, "%s", palabra) != EOF) {
         if (strcmp(palabra, "ecuador") == 0) {
             palabra[0] = toupper(palabra[0]);
+            //se imprime la palabra Ecuador 
             fprintf(archivoSalida, "%s ", palabra);
+            //contador de las faltas ortograficas
             conteoCorrecciones++;
         } else {
             fprintf(archivoSalida, "%s ", palabra);
         }
     }
-
+//se retorna a la variable conteo de correcciones
     return conteoCorrecciones;
 }
 
 //funcion main del programa
 int main() 
 {
+    char nombreArchivoEntrada[] = "tricolor.txt";
+    char nombreArchivoSalida[] = "tricolor corregido.txt";
+//Abrimos el archivo
+    FILE *archivoEntrada = fopen(nombreArchivoEntrada, "r");
+    FILE *archivoSalida = fopen(nombreArchivoSalida, "w");
+
+//sentencia condicional por si hay un error al abrir el archivo XD
+    if (archivoEntrada == NULL || archivoSalida == NULL) {
+        printf("Error al abrir los archivos.\n");
+        return 1;
+    }
+
+    int conteoTotal = CorregirOrtografia(archivoEntrada, archivoSalida);
+
+//en esta sentencia de imprecion se puede ver que tantas faltas ortograficas ubo
+    printf("Se corrigieron un total de %d palabras.\n", conteoTotal);
+
+//Cierre del archivo
+    fclose(archivoEntrada);
+    fclose(archivoSalida);
+    
     getch();
     return 0;
 }
