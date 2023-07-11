7259d618e029a1e33858914301efe6cd1cb4d5a7
Arie Quimi <trr.2002j@gmail.com>
2023-07-11 12:04:23 -0500

    primera estructura del programa

---
+++ programa.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+//funcion para corregir la falta
+void CorregirFalta()
+{
+
+}
+
+//funcion main del programa
+int main()
+{
+
+    getch();
+    return 0;
+}
